d3014f32e8e13729d9e0354a4c40659319c14bad
fade78 <fade@wanadoo.fr>
2012-03-26 09:46:15 +0200

    Further optimization to python simple processor 2 (tips from Winston Ewert)

--- python3/simpleprocessor2.py
+++ python3/simpleprocessor2.py
@@ -5,53 +5,59 @@ import sys
 import re
 import math
 
-SCRIPTTITLE="PYTHON SIMPLE PROCESSOR (regex parsing, optimization v1)"
+SCRIPTTITLE="PYTHON SIMPLE PROCESSOR (regex parsing, optimization v2)"
 
-linecount=0
-commentary=0
-unknownline=0
-data={}
+def main():
+    linecount=0
+    commentary=0
+    unknownline=0
+    data={}
 
-pattern_data=re.compile(r"^(\d+)\s+(\S+)\s+(\S+)\s+(\d+(?:\.\d+)?)")
+    write = sys.stdout.write
 
-print("#",SCRIPTTITLE,"\n#TRANSFORMED INPUT",sep='')
+    pattern_data=re.compile(r"^(\d+)\s+(\S+)\s+(\S+)\s+(\d+(?:\.\d+)?)")
 
-for line in sys.stdin:
-    linecount += 1
-    if line.startswith("#"):
-        commentary += 1
-        continue
-    line = line.replace(',','')
-    m = pattern_data.match(line)
-    if m:
-        i,k1,k2,value = m.groups()
-        i=int(i)
-        value=float(value)
-        try:
-            row = data[k1]
-        except KeyError:
-            row = data[k1] = {}
-        try:
-            row[k2] += value
-        except KeyError:
-            row[k2] = value
-        sys.stdout.write("{0},{1:.0f},{2},{3}\n".format(i,value,k2,k1))
-    else:
-        unknownline+=1
+    print("#",SCRIPTTITLE,"\n#TRANSFORMED INPUT",sep='')
 
-print("#DATADUMP")
+    for line in sys.stdin:
+        linecount += 1
+        if line.startswith("#"):
+            commentary += 1
+            continue
+        line = line.replace(',','')
+        m = pattern_data.match(line)
+        if m:
+            i,k1,k2,value = m.groups()
+            value=float(value)
+            try:
+                row = data[k1]
+            except KeyError:
+                row = data[k1] = {}
+            try:
+                row[k2] += value
+            except KeyError:
+                row[k2] = value
+            write(",".join([i, '{0:.0f}'.format(value), k2, k1])+"\n")
+        else:
+            unknownline+=1
 
-keystat=0
+    print("#DATADUMP")
 
-for k1, subkeys in sorted(data.items()):
-    sys.stdout.write(k1+':')
-    for k2,v in sorted(subkeys.items()):
-        keystat+=1
-        sys.stdout.write(' ({0}:{1:d})'.format(k2,math.trunc(v)))
-    sys.stdout.write("\n")
+    keystat=0
 
-report="#{0}\n#{1}\nparsed line: {2}, commentary line: {3}, unknown line: {4}, keystat: {5}.".format(
-               SCRIPTTITLE, sys.version.replace("\n"," "), linecount, commentary, unknownline, keystat)
+    for k1 in sorted(data):
+        subkeys = data[k1]
+        write(k1+':')
+        keystat += len(subkeys)
+        for k2 in sorted(subkeys):
+            v = subkeys[k2]
+            write(''.join([' (',k2,':',str(math.trunc(v)),')']))
+        write("\n")
 
-print("#REPORT\n"+report,file=sys.stdout)
-print(report,file=sys.stderr)
+    report="#{0}\n#{1}\nparsed line: {2}, commentary line: {3}, unknown line: {4}, keystat: {5}.".format(
+                   SCRIPTTITLE, sys.version.replace("\n"," "), linecount, commentary, unknownline, keystat)
+
+    print("#REPORT\n"+report,file=sys.stdout)
+    print(report,file=sys.stderr)
+
+main()
